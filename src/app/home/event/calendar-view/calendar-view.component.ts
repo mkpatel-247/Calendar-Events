f2613efcaadbfd1100cc6a54efc5abf813b0b5cc
mkpatel-247 <meet.codes247@gmail.com>
2024-06-19 16:10:48 +0530

    T314 Created List & Calendar Component

---
+++ src/app/home/event/calendar-view/calendar-view.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-calendar-view',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './calendar-view.component.html',
+  styleUrls: ['./calendar-view.component.scss']
+})
+export class CalendarViewComponent {
+
+}
--- src/app/home/event/event.component.ts
+++ src/app/home/event/event.component.ts
@@ -1,12 +1,23 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ListViewComponent } from './list-view/list-view.component';
+import { CalendarViewComponent } from './calendar-view/calendar-view.component';
 
 @Component({
   selector: 'app-event',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ListViewComponent, CalendarViewComponent],
   templateUrl: './event.component.html',
   styleUrls: ['./event.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class EventComponent {}
+export class EventComponent {
+  viewMode: boolean = false;
+
+  /**
+   * Toggle view list/calendar.
+   */
+  toggleViewButton() {
+    this.viewMode = !this.viewMode;
+  }
+}
